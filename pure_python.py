fd7214ac479819399962d9298e5231d78424e85d
Andrew Hearin <andrew.hearin@yale.edu>
2016-01-22 13:32:15 -0500

    Minor touchups to cython demo

--- pure_python.py
+++ pure_python.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+import numpy as np 
 
 def nth_prime_pure_python(nth_prime):
 
     k = 0
     n = 2
-    p = []
+    p = np.zeros(1000)
 
     while k < nth_prime:
         i = 0
         while i < k and n % p[i] != 0:
             i = i + 1
         if i == k:
-            p.append(n)
+            p[k] = n
             k = k + 1
             result = n
         n = n + 1
